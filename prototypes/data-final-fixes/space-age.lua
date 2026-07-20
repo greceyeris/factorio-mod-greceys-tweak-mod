4bfdbc6d198f76dc50c6d110807aa68c6b96e164
GreceyEris <143646777+greceyeris@users.noreply.github.com>
2026-07-20 15:24:15 +0800

    Bump version to 21.2.2; see changelog for details.

--- prototypes/data-final-fixes/space-age.lua
+++ prototypes/data-final-fixes/space-age.lua
@@ -3,7 +3,8 @@
 if mod_enabled("space-age") then
     -- 修复设置的问题
     -- 移除物品变质
-    if settings.startup["enable-remove-item-spoilage"].value then
+    if settings.startup["enable-remove-item-spoilage"] and
+        settings.startup["enable-remove-item-spoilage"].value then
         -- 遍历 raw
         for _, prototypes in pairs(data.raw) do
             -- 遍历 prototype
@@ -16,7 +17,8 @@ if mod_enabled("space-age") then
     end
 
     -- 移除机器冻结
-    if settings.startup["enable-remove-machine-freezing"].value then
+    if settings.startup["enable-remove-machine-freezing"] and
+        settings.startup["enable-remove-machine-freezing"].value then
         -- 遍历 raw
         for _, prototypes in pairs(data.raw) do
             -- 遍历 prototype
